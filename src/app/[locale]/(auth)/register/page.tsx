24a18c669b4fb35ffbc00d54c64813a7c1a760a6
michael <michaelsamy196@gmail.com>
2026-06-08 12:36:26 +0300

    refactor: improve formatting and structure of the RegisterPage component

--- src/app/[locale]/(auth)/register/page.tsx
+++ src/app/[locale]/(auth)/register/page.tsx
@@ -1,20 +1,31 @@
+'use client';
+
 import DecoratedText from '@/components/features/auth/decorated-text';
 import React from 'react';
 import RegisterForm from './_components/register-form';
-import Link from 'next/link';
+import { Link } from '@/i18n/navigation';
 import { useTranslations } from 'next-intl';
 
 export default function RegisterPage() {
   // Translations
   const t = useTranslations('pages.register');
 
-  return <div className='register-content'>
-    <DecoratedText text='register' className='mb-4' />
-    <RegisterForm />
-    <p className='text-center text-sm mt-5 rtl:font-tajawal font-medium text-zinc-800'>
-      {t.rich('have-account', {
-        Link: (chunks: string) => <Link href='/login' className='text-maroon-700 duration-300 hover:text-maroon-800'>{chunks}</Link>
-      })}
-    </p>
-  </div>;
+  return (
+    <div className="register-content">
+      <DecoratedText text="register" className="mb-4" />
+      <RegisterForm />
+      <p className="mt-5 text-center text-sm font-medium text-zinc-800 rtl:font-tajawal">
+        {t.rich('have-account', {
+          Link: (chunks: string) => (
+            <Link
+              href="/login"
+              className="text-maroon-700 duration-300 hover:text-maroon-800"
+            >
+              {chunks}
+            </Link>
+          ),
+        })}
+      </p>
+    </div>
+  );
 }
