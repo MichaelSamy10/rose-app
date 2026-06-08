8e636f7d936da5e5bd11abe99d9ea6917820abe7
michael <michaelsamy196@gmail.com>
2026-06-08 11:43:54 +0300

    feat: add utility to decode and retrieve NextAuth session token from cookies

--- src/lib/utils/get-token.ts
+++ src/lib/utils/get-token.ts
@@ -1,30 +1,24 @@
-'use server';
-
-import { encode, JWT } from 'next-auth/jwt';
+import { decode } from 'next-auth/jwt';
 import { cookies } from 'next/headers';
-// import { NEXTAUTH_COOKIE } from "../constants/auth.constant";
 
-export async function setToken(jwt: JWT) {
-  const encodedToken = await encode({
-    token: jwt,
-    secret: process.env.NEXTAUTH_SECRET!,
-    maxAge: 7 * 24 * 60 * 60, // 7 days,
-  });
-
-  cookies().set(
+export async function getToken() {
+  const cookieName =
     process.env.NODE_ENV === 'production'
       ? '__Secure-next-auth.session-token'
-      : 'next-auth.session-token',
-    encodedToken,
-    {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      maxAge: 7 * 24 * 60 * 60, // 7 days
-      path: '/',
-      sameSite: 'lax',
-      expires: new Date(
-        Date.now() + 7 * 24 * 60 * 60 * 1000,
-      ),
-    },
-  );
+      : 'next-auth.session-token';
+  const tokenCookie = cookies().get(cookieName)?.value;
+
+  if (!tokenCookie) return null;
+
+  try {
+    const jwt = await decode({
+      token: tokenCookie,
+      secret: process.env.NEXTAUTH_SECRET!,
+    });
+
+    return jwt;
+  } catch (error) {
+    console.error('Error decoding token', error);
+    return null;
+  }
 }
